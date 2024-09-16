7e153dae4796c51396b7adad80b8fe54a83eac4f
emperorbj <cwaku96@gmail.com>
2024-09-16 16:27:15 +0100

    error check 1

--- api/index.js
+++ api/index.js
@@ -7,12 +7,7 @@ app.use(cors({
   origin: ['https://scrib-eight.vercel.app'] // Your frontend's URL without the trailing slash
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://scrib-eight.vercel.app/');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 
 
 
