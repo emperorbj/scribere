0508caf73d42af7445ca2de3e1c40def23bac36a
emperorbj <cwaku96@gmail.com>
2024-09-16 14:35:14 +0100

    adjusted

--- api/index.js
+++ api/index.js
@@ -3,9 +3,18 @@ const cors = require('cors');
 const app = express();
 app.use(cors({
   credentials: true,
-  origin: 'https://scrib-eight.vercel.app',
+  origin: ['https://scrib-eight.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Your frontend's URL without the trailing slash
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://scrib-eight.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 const mongoose = require("mongoose");
 const User = require('./models/User');
 const Post = require('./models/Post');
